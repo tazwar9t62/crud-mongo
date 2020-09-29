16ee9a56cd829682a3227e1f5c3ac75c4e59b8ab
Tazwar <tazwar9t6@gmail.com>
2020-09-29 08:56:38 +0600

    cant fix the bug

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const MongoClient = require("mongodb").MongoClient;
 const uri =
   "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/9t6db?retryWrites=true&w=majority";
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-  res.send("Working");
+  res.sendFile(__dirname + "/index.html");
 });
 
 const client = new MongoClient(uri, {
@@ -17,17 +18,20 @@ const client = new MongoClient(uri, {
 });
 client.connect((err) => {
   const collection = client.db("9t6db").collection("hagus");
-  console.log("database successfully connected");
-  const data = {
-    name: "hagu",
-    color: "yellow",
-    price: 100,
-  };
-  collection.insertOne(data)
-  .then(res => {console.log("inserted one data")})
+  app.post("/addUser", (req, res) => {
+    let user1 = req.body;
+    console.log(user1);
+  });
+  //   console.log("database successfully connected");
+  //   const data = {
+  //     name: "hagu",
+  //     color: "yellow",
+  //     price: 100,
+  //   };
+  //   collection.insertOne(data)
+  //   .then(res => {console.log("inserted one data")})
 
   // perform actions on the collection object
-  
 });
 
 app.listen(3000, () => "this is running on port 3000");
