781debf824ba28dc308c3754773eb7612d7850e9
Tazwar <tazwar9t6@gmail.com>
2020-09-29 14:26:34 +0600

    cant fix the bug

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
 const uri =
   "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/9t6db?retryWrites=true&w=majority";
 const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -20,7 +18,24 @@ client.connect((err) => {
   const collection = client.db("9t6db").collection("hagus");
   app.post("/addUser", (req, res) => {
     let user1 = req.body;
-    console.log(user1);
+    collection.insertOne(user1).then((re) => {
+      console.log("data added succesfully");
+      res.send("success");
+    });
+  });
+  app.delete("/delete/:id", (req, res) => {
+    console.log(req.params.id);
+    collection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => console.log(result));
+  });
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+  app.get("/users", (req, res) => {
+    collection.find({}).toArray((err, document) => {
+      res.send(document);
+    });
   });
   //   console.log("database successfully connected");
   //   const data = {
